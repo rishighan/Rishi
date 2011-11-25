b13cb2967659b3087d1de3bde33727d4cffe4075
Rishi Ghan <rishi.ghan@gmail.com>
2011-11-25 11:27:11 -0500

    adding all the project files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rishi::Application.config.secret_token = 'f802c7b63bcf3f084980a3f2589feb5b64c0981e9165cca7c32c87d01d7f98785b014dea52bede0c2e29245e949d97f33d0dcd41deb4325023bc4810f4d9bd92'
