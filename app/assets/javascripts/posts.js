0aa6a9b4e60062f62b94e52bb9d129a38598fdc1
Rishi Ghan <rishi.ghan@gmail.com>
2012-01-19 18:44:17 -0500

    homework

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,10 +1,28 @@
-$('document').ready(function(){
-	
-	
-	$('form select').change(function(){
-		
-		
-	})
-	
-	
-})
+jQuery(function(){
+$("#insert_into_post").click(function() {
+  
+ var sumtin = $(this).prev().val();
+ var filename = 
+    
+});
+
+// Initialzes Create Reminder Modal Box
+// $("#new_reminder_dialog").dialog({
+    // autoOpen: false,
+    // height: 350,
+    // width: 350,
+    // modal: true,
+    // buttons: {
+        // 'Create New Reminder': function() {
+            // $('#new_reminder').submit();
+        // },
+        // Cancel: function() {
+            // $(this).dialog('close');
+        // }
+    // },
+    // close: function() {
+            // allFields.val('').removeClass('ui-state-error');
+    // }
+// });
+});
+
