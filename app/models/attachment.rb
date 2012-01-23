8341fe1c5e6fc1aaea4d35cd17150e6a0ac2c656
Rishi Ghan <rishi.ghan@gmail.com>
2012-01-22 22:19:26 -0500

    solving the paperclip :path and :url hassles.
    pending: missing.png snafus.

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,7 +3,8 @@ class Attachment < ActiveRecord::Base
   belongs_to :post
   
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-                    :path => ":rails_root/public/attachments/:style/:basename.:extension",
-                    :url => "attachments/:style/:basename.:extension"
+                    :url => "/pictures/:style/:basename.:extension",
+                    :path =>":rails_root/public/pictures/:style/:basename.:extension"
+                    
   
 end
