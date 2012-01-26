6d60f2eb5d0cce7a6bb89190817a1a6de9412e6b
Rishi Ghan <rishi.ghan@gmail.com>
2012-01-26 18:52:58 -0500

    regex matching for citation form.
    tricky tricky.

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,26 +1,37 @@
-function insertCarouselMarkup(){
-	
-	
+function insertCarouselMarkup() {
+
 	var textarea = $('#post_content');
 	var markup = '<div class="post-carousel"> </div>';
-	
-	textarea.val(textarea.val()+markup);
-	
+
+	textarea.val(textarea.val() + markup);
+
 }
 
 
-$(document).ready(function(){
-$('#post_category_id').change(function(){
+function insertSuperScript(identid)
+{
 	
-
-if($(this).val() == '1')
-$('#citations').fadeIn(300);
-
-else
-$('#citations').fadeOut(300);
+	// pattern for matching 
+	// ^(post_citations_attributes_[0-9]{1,3}_extended_description)$
+	var markup = '<sup> </sup>';
 	
-})
+	$(identid).val($(identid).val() + markup);
+	
+	
+}
 
+$(document).ready(function() {
+	$('#post_category_id').change(function() {
 
+		if($(this).val() == '1')
+			$('#citations').fadeIn(300);
+		
+		else
+		{
+			$('#citations :input').val('')
+			$('#citations').fadeOut(300);
+			
 
+		}
+	})
 })
