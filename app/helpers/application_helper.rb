532b55ad39344b3ecded39549cab1d613451a1c6
Rishi Ghan <rishi.ghan@gmail.com>
2012-02-06 00:51:43 -0500

    added messages from the future.
    The beast is coming. I am ready.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,8 @@ module ApplicationHelper
   end 
   
   # updated this to use pygments.rb instead of albino
+  # line numbers by passing an options hash
+  # http://pygments.org/docs/formatters/#formatter-classes
   def syntax_highlighter(html)  
     doc = Nokogiri::HTML(html)  
     doc.search("//pre[@lang]").each do |pre|  
