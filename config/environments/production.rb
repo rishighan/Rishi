4f2493e2e2a027377deeeb61cbc6498febdc965d
Rishi Ghan <rishi.ghan@gmail.com>
2012-02-09 15:36:50 -0500

    this

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rishi::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-   config.assets.precompile += ['vendor.js', 'jquery.js' ]
+   config.assets.precompile += ['vendor.js', 'jquery.js', 'jquery_ujs.js' ]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
