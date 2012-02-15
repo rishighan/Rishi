1d11e3c7dee31b1c4fca20444694427f0dbdc0c8
Rishi Ghan <rishi.ghan@gmail.com>
2012-02-14 19:01:13 -0500

    your turn, South Orange.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -48,6 +48,7 @@ class LineItemsController < ApplicationController
       if @line_item.save
         format.html { redirect_to (store_url)}
         format.json { render :json => @line_item, :status => :created, :location => @line_item }
+        format.js
       else
         format.html { render :action => "new" }
         format.json { render :json => @line_item.errors, :status => :unprocessable_entity }
