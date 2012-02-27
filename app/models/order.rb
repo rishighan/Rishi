192fa332bac2c2bca542fc4c9acc06e1507cc9c7
Rishi Ghan <rishi.ghan@gmail.com>
2012-02-27 08:19:31 -0500

    commented out some offending code.

--- app/models/order.rb
+++ app/models/order.rb
@@ -47,7 +47,7 @@ class Order < ActiveRecord::Base
   def purchase
     response = GATEWAY.purchase(price_in_cents, credit_card, purchase_options)
     transactions.create!(:action => "purchase", :amount => price_in_cents, :response => response)
-    cart.update_attribute(:purchased_at, Time.now) if response.success?
+    #cart.update_attribute(:purchased_at, Time.now) if response.success?
     response.success?
   end
 
