941c8dee0e0780479d4671431d173d3c422cdb79
Rishi Ghan <frishi@me.com>
2012-04-11 09:51:58 -0400

    work in progress on the hero carousel on the home page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -15,25 +15,34 @@ $(document).ready(function(){
 		  
 			pagerAnchorBuilder: function() {
 		        return '<a href="#" class="activeSlide"></a>';
-		    },
-			beforeFunction:function(){
-				alert('hi!');
-			}
-			
+		    }
 	   	});
 	 });
-        // 
-        //    $('.carousel-container').each(function() {
-        // 	var $nav = $('<div id="nav"></div>').insertAfter(this);
-        // 	
-        // 	
-        // 	$(this).cycle({
-        // 	        fx:     'scrollRight',
-        // 	        speed:   300,
-        // 	        timeout: 0,
-        // 	        pager:   $nav
-        // 	});
-        // });
+        
+           
+        	var $nav = $('<div id="hero-nav"></div>').insertAfter(this);
+        	
+        	
+        	$('.hero-module').cycle({
+        	        fx:     'fade',
+        	        speed:   300,
+        	        timeout: 0,
+        	        pager:   $nav,
+					before: populateBGImages()
+        	});
+
+			function populateBGImages(){
+				$('.hero-module img').each(function(index)
+				{
+					
+					//alert(index+': '+ $(this).attr('src'));
+					$('#mitra').replace('<span id= "'+index+'" style="background:url("'+$(this).attr('src')+'") 0 0 no-repeat;" ></span>');
+					$('#mitra').append('adas');
+				});
+			}
+				
+			
+      
  
     
  });
