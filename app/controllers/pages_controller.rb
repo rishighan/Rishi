a99535b200a30e76ed586daf8bb8c847a893c106
Rishi Ghan <rishi.ghan@gmail.com>
2012-05-03 16:55:07 -0400

    filtering drafts out from the archive page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -32,7 +32,7 @@ class PagesController < ApplicationController
 
   # GET /blog/archive
   def archive
-  	@posts = Post.exclude_category(["Home Carousel", "Colophon"])
+  	@posts = Post.exclude_category(["Home Carousel", "Colophon"]).without_draft.all
   	@post_months = @posts.group_by {|post| post.created_at.beginning_of_month}
     render :layout => "application"
   end
