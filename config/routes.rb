03ec2af98127a7e872bf9bcb7d78f992d88fcf5d
Rishi Ghan <rishi.ghan@gmail.com>
2012-05-03 01:07:03 -0400

    Todos and foodoos.

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rishi::Application.routes.draw do
   get 'blog/search' => 'pages#search', :as=> :blog_search
   
   # thesis section
+  # TODO: thesis/article
   get '/thesis' => 'pages#thesis'
   
   # projects
