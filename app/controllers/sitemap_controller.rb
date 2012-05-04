30d49ddc1310f77a03be99fc4c24239fe0820911
Rishi Ghan <rishi.ghan@gmail.com>
2012-05-04 13:36:40 -0400

    filtering out colophon from the site map.

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,7 +1,7 @@
 class SitemapController < ApplicationController
   def index
-    @urls = ['http://rishighan.com', 'http://rishighan.com/projects', 'http://rishighan.com/blog','http://rishighan.com/thesis']
-    @blog = Post.exclude_category(["Home Carousel","Projects"]).without_draft.all
+    @urls = ['http://rishighan.com', 'http://rishighan.com/projects', 'http://rishighan.com/blog','http://rishighan.com/thesis', 'http://rishighan.com/colophon']
+    @blog = Post.exclude_category(["Home Carousel","Projects", "Colophon"]).without_draft.all
     @projects = Post.include_category(['Projects']).without_draft.all
     @thesis = Post.include_category(['Thesis']).without_draft.all
     headers['content-type'] ='application/xml'
