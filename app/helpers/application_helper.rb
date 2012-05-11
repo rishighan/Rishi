4d15e802cdbf557a2406eb9ba58f9153014c5805
Rishi Ghan <rishi.ghan@gmail.com>
2012-05-11 13:50:46 -0400

    nokogiri acting up, adding some fixes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,11 +11,11 @@ module ApplicationHelper
   # http://pygments.org/docs/formatters/#formatter-classes
   # for line numbers add :options => {:linenos => 'inline'} to the Pygments call
   def syntax_highlighter(html)  
-    doc = Nokogiri::HTML::DocumentFragment.parse(html)  
+    doc = Nokogiri::HTML(html)  
     doc.search("//pre[@lang]").each do |pre|  
        pre.replace Pygments.highlight(pre.text.rstrip, :lexer => pre[:lang], :nowrap => true)  
     end  
-    doc.to_s  
+    doc.at_css("body").inner_html.to_s  
   end  
   
   #this is a helper method to hide the cart when its empty.
