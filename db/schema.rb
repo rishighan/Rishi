612e7f8523efaa6c2188f569bf67088c7d47cbb0
Rishi Ghan <frishi@me.com>
2012-09-23 23:24:01 -0400

    debugging after the upgrade.

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -18,28 +19,28 @@ ActiveRecord::Schema.define(:version => 20120418202338) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
   end
 
   create_table "attributions", :force => true do |t|
     t.integer  "variant_id"
     t.integer  "trait_id"
     t.string   "value"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "carts", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "categories", :force => true do |t|
     t.string   "category_name"
     t.string   "category_description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
   end
 
   create_table "categories_posts", :id => false, :force => true do |t|
@@ -51,14 +52,14 @@ ActiveRecord::Schema.define(:version => 20120418202338) do
     t.integer  "post_id"
     t.string   "source_name"
     t.text     "extended_description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
   end
 
   create_table "digitalfiles", :force => true do |t|
     t.integer  "variant_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
     t.string   "pack_file_name"
     t.string   "pack_content_type"
     t.integer  "pack_file_size"
@@ -79,8 +80,8 @@ ActiveRecord::Schema.define(:version => 20120418202338) do
   create_table "line_items", :force => true do |t|
     t.integer  "variant_id"
     t.integer  "cart_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
     t.integer  "quantity",   :default => 1
     t.integer  "order_id"
   end
@@ -93,8 +94,8 @@ ActiveRecord::Schema.define(:version => 20120418202338) do
     t.string   "authorization"
     t.string   "message"
     t.text     "params"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
   create_table "orders", :force => true do |t|
@@ -108,24 +109,24 @@ ActiveRecord::Schema.define(:version => 20120418202338) do
     t.string   "phone"
     t.string   "email"
     t.string   "pay_type"
+    t.date     "card_expires_on"
     t.string   "ip_address"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   create_table "post_drafts", :force => true do |t|
-    t.integer  "post_id"
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.string   "slug"
   end
 
@@ -134,8 +135,8 @@ ActiveRecord::Schema.define(:version => 20120418202338) do
   create_table "product_categories", :force => true do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "product_categories_products", :id => false, :force => true do |t|
@@ -144,20 +145,20 @@ ActiveRecord::Schema.define(:version => 20120418202338) do
   end
 
   create_table "product_shots", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer  "product_id"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
     t.string   "shot_file_name"
     t.string   "shot_content_type"
     t.integer  "shot_file_size"
     t.datetime "shot_updated_at"
-    t.integer  "product_id"
   end
 
   create_table "products", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "taggings", :force => true do |t|
@@ -180,23 +181,23 @@ ActiveRecord::Schema.define(:version => 20120418202338) do
   create_table "traits", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.boolean  "admin"
   end
 
@@ -209,8 +210,8 @@ ActiveRecord::Schema.define(:version => 20120418202338) do
     t.integer  "quantity"
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
   end
 
 end
